d3378132eeff2edf9c1714fadabb9ca7cc332753
V-2420 <V-2420@192.168.1.74>
2015-07-11 17:58:41 -0700

    data base.........

--- vemas/src/main/java/com/spaneos/vemas/util/DaoUtil.java
+++ vemas/src/main/java/com/spaneos/vemas/util/DaoUtil.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
-
+ 
 public class DaoUtil {
 	private Connection con;
 	private Statement stmt;
